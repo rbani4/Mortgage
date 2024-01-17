b5bd2b8d70a9b0088f8e6e2090094a83b84a8d3a
rbani4 <151563112+rbani4@users.noreply.github.com>
2024-01-17 16:52:49 -0500

    Update Main.java
    The program now opens on its login screen instead of the calculator.

--- Main.java
+++ Main.java
@@ -1,7 +1,21 @@
+import javax.swing.SwingUtilities;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.SwingUtilities;
+
 public class Main {
     public static void main(String[] args) {
-        MortgageCalculatorGUI gui = new MortgageCalculatorGUI();
-        gui.show();
-        aaa
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.setVisible(true);
+            }
+        });
     }
 }
