c4a01c472d46d10ba931ee6ac446a72e724b7d40
Sam <152322210+SamThapa006@users.noreply.github.com>
2024-01-31 11:35:16 -0500

    Update AmortizationEntry.java

--- AmortizationEntry.java
+++ AmortizationEntry.java
@@ -1,11 +1,12 @@
-import javax.swing.SwingUtilities;
 class AmortizationEntry {
+    // Private instance variables to store data
     private int month;
     private double monthlyPayment;
     private double principalPayment;
     private double interestPayment;
     private double remainingBalance;
 
+    // Constructor to initialize the object
     public AmortizationEntry(int month, double monthlyPayment, double principalPayment, double interestPayment, double remainingBalance) {
         this.month = month;
         this.monthlyPayment = monthlyPayment;
@@ -14,28 +15,29 @@ class AmortizationEntry {
         this.remainingBalance = remainingBalance;
     }
 
-    @Override
+    // Method to format and return a string representation of the object
     public String toString() {
-        return String.format("%-7d%-10.2f%-11.2f%-11.2f%-16.2f", month, monthlyPayment, principalPayment, interestPayment, remainingBalance);
+        return String.format("%-7d%-10.2f%-11.2f%-11.2f%-16.2f", this.month, this.monthlyPayment, this.principalPayment, this.interestPayment, this.remainingBalance);
     }
 
+    // Getter methods to access private instance variables
     public int getMonth() {
-        return month;
+        return this.month;
     }
 
     public double getRemainingBalance() {
-        return remainingBalance;
+        return this.remainingBalance;
     }
 
     public double getInterestPayment() {
-        return interestPayment;
+        return this.interestPayment;
     }
 
     public double getPrincipalPayment() {
-        return principalPayment;
+        return this.principalPayment;
     }
 
     public double getMonthlyPayment() {
-        return monthlyPayment;
+        return this.monthlyPayment;
     }
 }
